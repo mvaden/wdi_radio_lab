4def55f1f9c307b5567d22c85589aa6eac887256
Matt <mvaden08@gmail.com>
2016-05-05 15:21:48 -0400

    added router functionality, templateUrl, to app.js.

--- js/app.js
+++ js/app.js
@@ -1,44 +1,18 @@
-"use strict"
+"use strict";
 
-function(){
+(function(){
   angular
-    .module("wdiRadio",[
-      "ui.router"
-    ])
-    .config("somename2",[
+    .module("wdiRadio", ["ui.router"]);
+    .config([
       "$stateProvider",
-      RouterFunc
-    ])
+      RouterFunction
+    ]);
 
-    function RouterFunc($stateProvider){
+    function RouterFunction($stateProvider){
       $stateProvider
-      .state("home", {
-        url: "",
-        template: "Checkout my favorite trackz!!!"
-      })
-      .state("songs", {
-        url: "/songs",
-        templateUrl: "js/songs/index.html",
-        controller: "SongsIndexController",
-        controllerAs: "SongsIndexViewModel"
-      })
-      .state("newSong", {
-        url: "/songs/new",
-        templateUrl: "js/songs/new.html",
-        controller: "SongsNewController",
-        controllerAs: "SongsNewViewModel"
-      })
-      .state("songsShow", {
-        url: "/songs/:id",
-        templateUrl: "js/songs/show.html",
-        controller: "SongsShowController",
-        controllerAs: "SongsShowViewModel"
-      })
-      .state("editSong", {
-        url: "/songs/:id/edit",
-        templateUrl: "js/songs/edit.html",
-        controller: "SongsEditController",
-        controllerAs: "SongsEditViewModel"
+      .state("homepage", {
+        url:"/index",
+        templateUrl: "js/songs/index.html"
       });
-    };
-}());
+    }
+})();
