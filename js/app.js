2fe383d094ee855d3e2e192b512da911405bc5dc
Matt <mvaden08@gmail.com>
2016-05-05 11:25:44 -0400

    Added router functionality to app.js with states.

---
+++ js/app.js
@@ -0,0 +1,44 @@
+"use strict"
+
+function(){
+  angular
+    .module("somename1",[
+      "ui.router"
+    ])
+    .config("somename2",[
+      "$stateProvider",
+      RouterFunc
+    ])
+
+    function RouterFunc($stateProvider){
+      $stateProvider
+      .state("home", {
+        url: "",
+        template: "Checkout my favorite trackz!!!"
+      })
+      .state("songs", {
+        url: "/songs",
+        templateUrl: "js/songs/index.html",
+        controller: "SongsIndexController",
+        controllerAs: "SongsIndexViewModel"
+      })
+      .state("newSong", {
+        url: "/songs/new",
+        templateUrl: "js/songs/new.html",
+        controller: "SongsNewController",
+        controllerAs: "SongsNewViewModel"
+      })
+      .state("songsShow", {
+        url: "/songs/:id",
+        templateUrl: "js/songs/show.html",
+        controller: "SongsShowController",
+        controllerAs: "SongsShowViewModel"
+      })
+      .state("editSong", {
+        url: "/songs/:id/edit",
+        templateUrl: "js/songs/edit.html",
+        controller: "SongsEditController",
+        controllerAs: "SongsEditViewModel"
+      })
+    }
+}
